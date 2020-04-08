15046054ea8f23627a082ccaa12d98b811ed27be
theo39 <dawarsardar786@gmail.com>
2020-04-08 18:19:28 +0500

    change

--- apps/views.py
+++ apps/views.py
@@ -471,7 +471,7 @@ def add_to_fb(name,passs,post_content):
     opt.add_argument('--headless')
     opt.add_argument('--no-sandbox')
     opt.add_experimental_option('excludeSwitches',['enable-automation'])
-    driver = webdriver.Chrome(executable_path=cd_url,options=opt)
+    driver = webdriver.Chrome(options=opt)
     driver.get('https://web.facebook.com/')
     # start login
     while True:
@@ -514,7 +514,7 @@ def add_to_twitter(name,passs,post_content,verify):
     opt.add_argument('--headless')
     opt.add_argument('--no-sandbox')
     opt.add_experimental_option('excludeSwitches',['enable-automation'])
-    driver = webdriver.Chrome(executable_path=cd_url,options=opt)
+    driver = webdriver.Chrome(options=opt)
     driver.get('https://twitter.com/login')
     # start login
     while True:
@@ -563,7 +563,7 @@ def post_to_fb_page(name,passs,post_content,page_name):
     opt.add_argument('--headless')
     opt.add_argument('--no-sandbox')
     opt.add_experimental_option('excludeSwitches',['enable-automation'])
-    driver = webdriver.Chrome(executable_path=cd_url,options=opt)
+    driver = webdriver.Chrome(options=opt)
     driver.get('https://mbasic.facebook.com/'+page_name+'/')
     time.sleep(5)
     driver.find_element_by_xpath('//*[@id="mobile_login_bar"]/div[2]/a[2]').click()
